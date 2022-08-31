e8dd10381c6f2131e79e5a5b394120f6c1effd48
Márton Kardos <power.up1163@gmail.com>
2022-08-31 16:49:19 +0200

    Added setup.py

---
+++ tweetopic/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Tweetopic",
+    version="0.0.1",
+    description="Efficient Numba based implementation of Gibbs Sampling Dirichlet Mixed Model.",
+    author="Márton Kardos",
+    author_email="power.up1163@gmail.com",
+    packages=["tweetopic"],
+)
